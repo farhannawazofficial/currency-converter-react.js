3335d5c24ed504910d4dc32d943b7469fddb9072
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2024-11-25 12:01:27 +0500

    added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import './test.scss';
 
 const App = () => {
   return (
