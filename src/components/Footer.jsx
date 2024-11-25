3923bae0e590aec727323eb1c9ca136dff90e3c1
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2024-11-25 12:06:07 +0500

    new added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>© 2024 Currency Converter. All rights reserved.</p>
+      <p>© 2024 Currency Converter. All rights reserved. Created by <br /> <b>Farhan Nawaz</b> </p>
     </footer>
   );
 };
